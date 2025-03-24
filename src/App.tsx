4fed32115ee56f2237b783c3bb9854e39fd33fe8
Samraffi <samvel.vardanyan.developer@gmail.com>
2025-03-24 10:18:06 +0400

    feat: initialize React application with Tailwind CSS and Webpack setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div className="App">Simple React Blog</div>
+  );
+}
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,63 @@
+// webpack.config.ts
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import webpack from 'webpack';
+import 'webpack-dev-server';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const config: webpack.Configuration = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "./main.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+};
+
+export default config;
